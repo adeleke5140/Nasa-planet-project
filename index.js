cea2ca013b241caa4d0f7b47aca2810d7760e07e
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-09 15:45:12 +0100

    :sparkles: add function to filter habitable planets
    filter the planets data stream to only display the habitable planets fitting certain characters

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const { parse } = require("csv-parse")
 const fs = require("fs")
 
-const results = []
+const habitablePlanets = []
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  )
+}
 
 //create a readStream by using the file system module
+//we pipe our pass parse function to the stream
 fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true
+    })
+  )
   .on("data", (data) => {
-    results.push(data)
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data)
+    }
   })
   .on("error", () => {
     console.log(err)
   })
   .on("end", () => {
-    console.log(results)
-    console.log("done")
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet.kepler_name
+      })
+    )
+    console.log(`${habitablePlanets.length} habitable planets found!✨`)
   })
+
+//we get buffer of data but we need to parse it
